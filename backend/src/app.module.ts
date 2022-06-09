a87261dfb72a9bccd4ccfc56ef4ac12c98856f70
gangukss <kwk7328@gmail.com>
2022-06-10 03:20:37 +0900

    [BE] FEAT: checking  42api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeORMConfig } from './configs/typeorm.config';
 import { UserModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeORMConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(TypeORMConfig), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
