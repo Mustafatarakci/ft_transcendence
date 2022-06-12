74a13e35a81fb642bdbea10a2c8f0fe2ec0af1f9
gangukss <kwk7328@gmail.com>
2022-06-13 03:44:22 +0900

    [BE] FEAT: add all-exceptions filter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,11 +6,19 @@ import { TypeORMConfig } from './configs/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggerMiddleware } from './logger.middleware';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(TypeORMConfig), UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
