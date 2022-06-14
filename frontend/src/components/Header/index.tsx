e4eaa6c584a248ce88d23029a68ee8860e800fad
som-syom <dhyeon@student.42seoul.kr>
2022-06-14 16:32:00 +0900

    [FE #12] FEAT: Header에 라우팅 추가, 태그변경

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import LogoImg from '../../assets/logo-white.png';
+import { MenuType, GAME, CHAT } from '../../utils/interface';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  onClickMenu: (menuType: MenuType) => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onClickMenu }) => {
   return (
     <HeaderContainer>
-      <LogoWrap>
+      <LogoWrap onClick={() => onClickMenu(GAME)}>
         <Logo src={LogoImg} alt="Home" />
       </LogoWrap>
       <Menus>
-        <Menu>GAME</Menu>
-        <Menu>CHAT</Menu>
+        <Menu onClick={() => onClickMenu(GAME)}>GAME</Menu>
+        <Menu onClick={() => onClickMenu(CHAT)}>CHAT</Menu>
       </Menus>
     </HeaderContainer>
   );
 };
 
-const HeaderContainer = styled.div`
+const HeaderContainer = styled.header`
   width: 100%;
   height: 100px;
   display: flex;
@@ -36,10 +41,11 @@ const Logo = styled.img`
 
   cursor: pointer;
 `;
-const Menus = styled.ul`
+const Menus = styled.nav`
   display: flex;
 `;
-const Menu = styled.li`
+const Menu = styled.span`
+  display: inline-block;
   width: 100px;
   color: white;
   font-size: 20px;
