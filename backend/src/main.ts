83ccbd7aedf27c3ae19c2dd83127e8a1d881f048
Kim KangWook <59438936+gangukss@users.noreply.github.com>
2022-06-21 01:27:34 +0900

    Revert "[BE] Feat. docker-compose with Backend, DB (#50)"
    This reverts commit bc18bda32729de4b805d9f55d110f48f0261bc1f.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dayjs from 'dayjs';
-import * as utc from 'dayjs/plugin/utc';
-import * as timezone from 'dayjs/plugin/timezone';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.tz.setDefault('Asia/Seoul');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('42-Tomodachi')
-    .setDescription(`42-Tomodachi's transcendence API description`)
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(5500);
+  await app.listen(3000);
 }
 bootstrap();
