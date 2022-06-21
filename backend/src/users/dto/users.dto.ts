a428efa2731f1c173f838cf8191ad7ffa7ef7267
gangukss <kwk7328@gmail.com>
2022-06-22 03:41:07 +0900

    [BE] ChORE: UserProfileDto 설명 추가

--- backend/src/users/dto/users.dto.ts
+++ backend/src/users/dto/users.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsEmail, IsString } from 'class-validator';
+import { IsBoolean, IsEmail, IsNumber, IsString } from 'class-validator';
 
 export class UserDataDto {
   @ApiProperty({ description: '아바타' })
@@ -34,12 +34,27 @@ export class EmailDto {
 }
 
 export class UserProfileDto {
+  @ApiProperty({ description: '유저 id' })
   id: number;
+
+  @ApiProperty({ description: '유저 닉네임' })
   nickname: string;
+
+  @ApiProperty({ description: '유저 아바타' })
   avatar: string;
+
+  @ApiProperty({ description: '유저 이메일' })
   email: string;
+
+  @ApiProperty({ description: '일반게임 승리 카운트' })
   winCount: number;
+
+  @ApiProperty({ description: '일반게임 패배 카운트' })
   loseCount: number;
+
+  @ApiProperty({ description: '래더게임 승리 카운트' })
   ladderWinCount: number;
+
+  @ApiProperty({ description: '래임게임 패배 카운트' })
   ladderLoseCount: number;
 }
