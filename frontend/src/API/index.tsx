58087ca27b2836e04dbc1e64c1351c76bb822696
som-syom <dhyeon@student.42seoul.kr>
2022-06-25 18:24:02 +0900

    [FE] CHORE: instance, default 양식 추가

--- frontend/src/API/index.tsx
+++ frontend/src/API/index.tsx
@@ -1,1 +1,33 @@
+import axios from 'axios';
+
+const baseURL = process.env.BACK_URL;
+
+export const instance = axios.create({
+  baseURL,
+});
+
+/* NOTE: Default 양식
+
+=========
+import axios from 'axios';
+import instance from '.';
+
+const authPath = path => {
+  return `/auth${path}`;
+};
+
+const authAPI = {
+  isSignedUp: async (body) => {
+		try {
+			const url = authPath(`/isSignedUp`);
+			response = await instance.post(url, body);
+			return response;
+		} catch (e: Error) {
+			console.log(e.message);
+		}
+  },
+}
+
+*/
+
 export {};
