9bf2b53ff35a83556bfee6885c45ad660c6f03a9
som-syom <dhyeon@student.42seoul.kr>
2022-06-26 18:50:45 +0900

    [FE] CHORE: API default 양식 최종 업뎃2

--- frontend/src/API/index.tsx
+++ frontend/src/API/index.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { authAPI } from './auth';
 
-// const url = 'http://ec2-15-164-215-143.ap-northeast-2.compute.amazonaws.com:5500';
 const url = process.env.REACT_APP_BACK_API;
 
 export const instance = axios.create({
@@ -11,23 +10,28 @@ export const instance = axios.create({
 /* NOTE: Default 양식
 
 =========
-import {instance} from './index';
+import { IUser } from '../utils/interface';
+import { instance } from './index';
 
 const authPath = (path: string) => {
   return `/auth${path}`;
 };
 
-const authAPI = {
-  isSignedUp: async (body: {code: string}) => {
-		try {
-			const url = authPath(`/isSignedUp`);
-			const response = await instance.post(url, body);
-			return response;
-		} catch (e: any) {
-			console.log(e.message);
-		}
+export const authAPI = {
+  isSignedUp: async (body: { code: string }): Promise<IUser | null> => {
+    try {
+      const url = authPath(`/isSignedUp`);
+      const response = await instance.post(url, body);
+      return response.data;
+    } catch (e: any) {
+      console.log(e.message);
+      return null;
+    }
   },
-}
+ 
+};
+
+
 
 */
 
